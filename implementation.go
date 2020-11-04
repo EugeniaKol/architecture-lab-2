60fbcf40a64fce6ead53c522d5abc7e065ae5734
EugeniaKol <jennichka10155@gmail.com>
2020-11-04 20:10:42 +0200

    adding implementation and test of ToInfix

--- implementation.go
+++ implementation.go
@@ -1,9 +1,68 @@
 package lab2
 
-import "fmt"
+import (
+    "bufio"
+    "io"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+type Stack[] string
+
+type Exp struct{
+    Input io.Reader
 }
+
+func IsOperand(s string) bool{
+    for _, r := range s {
+        if (r < 'a' || r > 'z') && (r < 'A' || r > 'Z') && (r < '0' || r > '9') {
+            return false
+        }
+    }
+    return true
+}
+
+func IsOperator(s string) bool{
+    if s == "+" || s == "-" || s == "*" || s == "^" || s == "/"{
+        return true
+    }
+    return false
+}
+
+func (exp *Exp) ToInfix(output chan string) {
+    scanner := bufio.NewScanner(exp.Input)
+    scanner.Split(bufio.ScanWords)
+    var stack Stack
+
+    for scanner.Scan() {
+        item := scanner.Text()
+        if IsOperand(item) {
+            stack = append(stack, item)
+
+        } else if IsOperator(item) {
+            if len(stack) < 2 {
+                output <- "incorrect number of operands"
+                close(output)
+                return
+            }
+            op1 := stack[len(stack)-1]
+            stack = stack[:len(stack)-1]
+            op2 := stack[len(stack)-1]
+
+            stack = stack[:len(stack)-1]
+            newItem := "(" + op2 + " " + item + " " + op1 + ")"
+            stack = append(stack, newItem)
+
+        } else {
+            output <- "forbidden symbol at: "
+            output <- item
+            close(output)
+            return
+        }
+    }
+    if len(stack) == 1 {
+        output <- stack[0]
+    } else if len(stack) == 0 {
+        output <- "empty input"
+    }
+    close(output)
+}
+
--- implementation_test.go
+++ implementation_test.go
@@ -1,23 +1,82 @@
 package lab2
 
 import (
-	"fmt"
-	"testing"
-
-	"github.com/stretchr/testify/assert"
+    . "gopkg.in/check.v1"
+    "strings"
+    "testing"
+    "fmt"
 )
 
-func TestPrefixToPostfix(t *testing.T) {
-	res, err := PrefixToPostfix("+ 5 * - 4 2 3")
-	if assert.Nil(t, err) {
-		assert.Equal(t, "4 2 - 3 * 5 +", res)
-	}
+func Test(t *testing.T) { TestingT(t) }
+
+type TestSuite struct{}
+
+var _ = Suite(&TestSuite{})
+
+func (s * TestSuite) TestToInfix(c *C) {
+    for _, testCase := range []struct{
+        dummy string
+        expected string
+    }{
+        {
+            dummy: "",
+            expected: "empty input",
+        },
+        {
+            dummy: "a - b *",
+            expected: "incorrect number of operands",
+        },
+        {
+            dummy: "C 16 - a *",
+            expected: "((C - 16) * a)",
+        },
+        {
+            dummy: "a b * c ^ d /",
+            expected: "(((a * b) ^ c) / d)",
+        },
+        {
+            dummy: "5 73 + 20 t ^ /",
+            expected: "((5 + 73) / (20 ^ t))",
+        },
+        {
+            dummy: "5 6 d * -  T 30 * ^ c +",
+            expected: "(((5 - (6 * d)) ^ (T * 30)) + c)",
+        },
+        {
+            dummy: "a b ^ c * d - e f / g h + / +",
+            expected: "((((a ^ b) * c) - d) + ((e / f) / (g + h)))",
+        },
+
+        {
+            dummy: "a b# + c -",
+            expected: "forbidden symbol at: b#",
+        },
+    }{
+        exp := &Exp{Input: strings.NewReader(testCase.dummy)}
+
+        output := make(chan string)
+        go exp.ToInfix(output)
+
+        var res string
+        for data := range output{
+            res = res + data
+        }
+
+        c.Assert(res, Equals, testCase.expected)
+    }
 }
 
-func ExamplePrefixToPostfix() {
-	res, _ := PrefixToPostfix("+ 2 2")
-	fmt.Println(res)
+func ExampleExp_ToInfix() {
+    exp := &Exp{Input: strings.NewReader("a b c * +")}
 
-	// Output:
-	// 2 2 +
+    output := make(chan string)
+    go exp.ToInfix(output)
+
+    var res string
+    for data := range output{
+        res = res + data
+    }
+
+    fmt.Println(res)
+    // Output: (a + (b * c))
 }
