ae7218c1d95e3e87773ee3dd5a278909dfefa295
Daria Diakonenko <daria.diakonenko@gmail.com>
2020-11-05 18:26:44 +0200

    draft handler. go and main.go

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -2,18 +2,61 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"strings"
+
 	lab2 "github.com/roman-mazur/architecture-lab-2"
 )
 
 var (
 	inputExpression = flag.String("e", "", "Expression to compute")
 	// TODO: Add other flags support for input and output configuration.
+	inputFile  = flag.String("f", "", "Input file")
+	outputFile = flag.String("o", "", "Output file")
 )
 
 func main() {
 	flag.Parse()
 
+	var src io.Reader
+	var dest io.Writer
+
+	if *inputExpression != "" {
+		src = strings.NewReader(*inputExpression)
+	} else if *inputFile != "" {
+		data, err := ioutil.ReadFile(*inputFile)
+		if err != nil {
+			_, _ = os.Stderr.WriteString(err.Error())
+			return
+		}
+		src = strings.NewReader(string(data))
+	} else {
+		_, _ = os.Stderr.WriteString("error: no expression provided")
+		return
+	}
+
+	if *outputFile != "" {
+		if file, err := os.Create(*outputFile); err == nil {
+			dest = file
+		} else {
+			_, _ = os.Stderr.WriteString(err.Error())
+			return
+		}
+	} else {
+		dest = os.Stdout
+	}
+
+	handler := lab2.ComputeHandler{
+		Input:  src,
+		Output: dest,
+	}
+
+	if err := handler.Compute(); err != nil {
+		_, _ = os.Stderr.WriteString(err.Error())
+		return
+	}
 	// TODO: Change this to accept input from the command line arguments as described in the task and
 	//       output the results using the ComputeHandler instance.
 	//       handler := &lab2.ComputeHandler{
@@ -22,6 +65,12 @@ func main() {
 	//       }
 	//       err := handler.Compute()
 
-	res, _ := lab2.PrefixToPostfix("+ 2 2")
-	fmt.Println(res)
+	// handler := &lab2.ComputeHandler{
+	// 	Input:  src,
+	// 	Output: dest,
+	// }
+
+	// err := handler.Compute()
+	// res, _ := lab2.ToInfix()
+	// fmt.Println(res)
 }
--- go.mod
+++ go.mod
@@ -3,5 +3,8 @@ module github.com/roman-mazur/architecture-lab-2
 
 go 1.15
 
-// TODO: Change if need to use a different assertions library.
-require github.com/stretchr/testify v1.6.1
+require (
+	// TODO: Change if need to use a different assertions library.
+	github.com/stretchr/testify v1.6.1
+	gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405
+)
